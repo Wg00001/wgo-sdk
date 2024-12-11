a2cdd1e849c1cb6c6dbb08ac4bd58de34a1c7517
warren <warrenwu@soarinfotech.com>
2024-12-11 14:41:25 +0800

    slice 2

--- wg/slice.go
+++ wg/slice.go
@@ -1,19 +1,46 @@
 package wg
 
-var SliceCapFunc = func(originLen int) int {
+// DefaultSliceCap slice转换目标的默认cap,用户可以直接修改此函数,以根据需要来修改默认值,使性能更符合需求
+var DefaultSliceCap = func(originLen int) int {
 	return originLen / 5 * 4 //默认cap是原len的80%
 }
 
-func SliceToMap[K comparable, V any](slice []V, getKey func(item V) K) map[K]V {
-	res := make(map[K]V, SliceCapFunc(len(slice)))
-	for i := range slice {
-		res[getKey(slice[i])] = slice[i]
+// SliceToMap 将slice转成map,需要传入函数用于获取Key和Value
+func SliceToMap[K comparable, V, T any](slice []T, getKeyValue func(item T) (K, V)) map[K]V {
+	res := make(map[K]V, DefaultSliceCap(len(slice)))
+	for _, item := range slice {
+		k, v := getKeyValue(item)
+		res[k] = v
 	}
 	return res
 }
 
-func SliceToMapGroup[K comparable, V any](slice []V, getKeyGroupBy func(item V) K) map[K][]V {
-	res := make(map[K][]V, SliceCapFunc(len(slice)))
+// SliceToIndex 使用map对slice进行索引,需要传入函数获得索引值
+func SliceToIndex[K comparable, V any](slice []V, getKey func(item V) K) map[K]V {
+	res := make(map[K]V, DefaultSliceCap(len(slice)))
+	for _, item := range slice {
+		res[getKey(item)] = item
+	}
+	return res
+}
+
+// SliceToMapGroup 将slice转成map,但是索引冲突时不覆盖而是保存成数组
+func SliceToMapGroup[K comparable, V, T any](slice []T, getKeyGroupBy func(item T) (K, V)) map[K][]V {
+	res := make(map[K][]V, DefaultSliceCap(len(slice)))
+	for _, item := range slice {
+		key, value := getKeyGroupBy(item)
+		if _, ok := res[key]; !ok {
+			res[key] = []V{value}
+		} else {
+			res[key] = append(res[key], value)
+		}
+	}
+	return res
+}
+
+// SliceToIndexGroup 使用map对slice进行索引,但是索引冲突时不覆盖而是保存成数组
+func SliceToIndexGroup[K comparable, V any](slice []V, getKeyGroupBy func(item V) K) map[K][]V {
+	res := make(map[K][]V, DefaultSliceCap(len(slice)))
 	for i := range slice {
 		key := getKeyGroupBy(slice[i])
 		if _, ok := res[key]; !ok {
@@ -25,28 +52,31 @@ func SliceToMapGroup[K comparable, V any](slice []V, getKeyGroupBy func(item V) 
 	return res
 }
 
+// SliceToSet 将slice转成set,即value的类型为空struct的map
 func SliceToSet[K comparable, T any](slice []T, getKey func(item T) K) map[K]struct{} {
-	res := make(map[K]struct{}, SliceCapFunc(len(slice)))
+	res := make(map[K]struct{}, DefaultSliceCap(len(slice)))
 	for i := range slice {
 		res[getKey(slice[i])] = struct{}{}
 	}
 	return res
 }
 
-func SliceToSlice[T any, U any](slice []T, getResultSliceItem func(item T) U) []U {
+// SliceToSlice 根据此Slice转换成另一个Slice,需要传入函数返回结果slice的元素值
+func SliceToSlice[T, U any](slice []T, getResultSliceItem func(item T) U) []U {
 	if slice == nil || len(slice) == 0 {
 		return []U{}
 	}
-	res := make([]U, 0, SliceCapFunc(len(slice)))
+	res := make([]U, 0, DefaultSliceCap(len(slice)))
 	for _, item := range slice {
 		res = append(res, getResultSliceItem(item))
 	}
 	return res
 }
 
+// SliceUnique 数组去重
 func SliceUnique[E comparable](slice []E) []E {
 	var res []E
-	memo := make(map[E]struct{}, SliceCapFunc(len(slice)))
+	memo := make(map[E]struct{}, DefaultSliceCap(len(slice)))
 	for i := range slice {
 		if _, ok := memo[slice[i]]; !ok {
 			res = append(res, slice[i])
@@ -56,6 +86,7 @@ func SliceUnique[E comparable](slice []E) []E {
 	return res
 }
 
+// SliceChunk 将slice进行分片,变成二维数组
 func SliceChunk[T any](slice []T, size int) [][]T {
 	n := len(slice)
 	if n < size {
@@ -68,8 +99,9 @@ func SliceChunk[T any](slice []T, size int) [][]T {
 	return res
 }
 
+// SliceFilter 过滤slice中的元素,当传入函数的返回值为false将该元素过滤
 func SliceFilter[T any](slice []T, isOk func(item T) bool) []T {
-	res := make([]T, 0, SliceCapFunc(len(slice)))
+	res := make([]T, 0, DefaultSliceCap(len(slice)))
 	for _, item := range slice {
 		if isOk(item) {
 			res = append(res, item)
